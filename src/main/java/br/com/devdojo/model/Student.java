5507ea4b5e51a024c094730002554725c6cc1e54
Roberto Bomfim <bomfim062@gmail.com>
2022-09-14 11:54:08 -0300

    Aula 14/09/22.

--- src/main/java/br/com/devdojo/model/Student.java
+++ src/main/java/br/com/devdojo/model/Student.java
@@ -1,13 +1,57 @@
 package br.com.devdojo.model;
 
+import br.com.devdojo.endpoint.StudentEndpoint;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static java.util.Arrays.asList;
+
 public class Student {
+    private int id;
+    public static List<Student> studentList;
     private String name;
+
+    static {
+        studentRepository();
+    }
+
+    public Student(int id, String name) {
+        this(name);
+        this.id = id;
+    }
 
     public Student(String name) {
         this.name = name;
     }
 
     public Student() {
+    }
+
+    private static void studentRepository() {
+        studentList = new ArrayList<>(asList(new Student(1,"Naruto"), new Student(2, "Sasuke")));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
